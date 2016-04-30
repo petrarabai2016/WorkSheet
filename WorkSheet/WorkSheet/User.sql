53a76e46b7096c6fc4e89fafe7e4ff328744c718
Petra Rábai <petrarabai.developer@gmail.com>
2016-04-30 16:32:51 +0200

    Create user table

---
+++ WorkSheet/WorkSheet/User.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FullName] NVARCHAR(50) NOT NULL, 
+    [LoginName] NCHAR(10) NOT NULL, 
+    [Password] NCHAR(10) NOT NULL, 
+    [Region] BIT NOT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Store login name',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'LoginName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Store user full name',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'FullName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Store user password',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'Password'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Store user is in region or not',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'User',
+    @level2type = N'COLUMN',
+    @level2name = N'Region'
